ce45cd77cf3357df6486464ac4d9d5a499c38a17
galaxies99 <galaxies@vip.qq.com>
2019-04-30 15:23:32 +0800

    initial update

---
+++ BplusTree/cmake-build-debug/CMakeFiles/BplusTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BplusTree.dir/main.cpp.obj"
+  "BplusTree.pdb"
+  "BplusTree.exe"
+  "BplusTree.exe.manifest"
+  "libBplusTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BplusTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
